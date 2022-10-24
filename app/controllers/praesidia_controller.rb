e3ef337c66e8af3e9a93454872b8a8b7464a08b4
John Ashaba <jnashaba4@gmail.com>
2022-10-24 22:01:27 +0300

    add praesidium views and functionality

--- app/controllers/praesidia_controller.rb
+++ app/controllers/praesidia_controller.rb
@@ -1,2 +1,50 @@
 class PraesidiaController < ApplicationController
+  def index
+    @praesidia = Praesidium.all
+  end
+
+  def show
+    @praesidium = Praesidium.find(params[:id])
+  end
+
+  def new
+    @praesidium = Praesidium.new
+  end
+
+  def create
+    @praesidium = Praesidium.new(praesidium_params)
+
+    if @praesidium.save
+      redirect_to @praesidium
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @praesidium = Praesidium.find(params[:id])
+  end
+
+  def update
+    @praesidium = Praesidium.find(params[:id])
+
+    if @praesidium.update(praesidium_params)
+      redirect_to @praesidium
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @praesidium = Praesidium.find(params[:id])
+    @praesidium.destroy
+
+    redirect_to praesidia_path, status: :see_other
+  end
+
+  private
+
+  def praesidium_params
+    params.require(:praesidium).permit(:name, :parish, :curia_id, :date_created)
+  end
 end
--- app/models/praesidium.rb
+++ app/models/praesidium.rb
@@ -1,3 +1,9 @@
 class Praesidium < ApplicationRecord
   belongs_to :curia
+
+  validates :name, presence: true
+  validates :parish, presence: true
+  validates :date_created, presence: true
+
+  validates_associated :curia
 end
